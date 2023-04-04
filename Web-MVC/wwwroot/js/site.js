ef264461a89a19530b3b518387e3fb23d28184fc
sin-2-x <silantiys@mail.ru>
2023-04-04 23:54:50 +0300

    добавлено окно показа списка трат при клике на день

--- Web-MVC/wwwroot/js/site.js
+++ Web-MVC/wwwroot/js/site.js
@@ -2,8 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let calendarDate = {};
-let popup = document.querySelector(".popup");
+let calendar = document.querySelector(".calendar");
+
+let popupWasteList = document.querySelector(".popup_type_waste-list");
+popupWasteList.querySelector(".popup__dackground").addEventListener('click', closePopupWasteList);
+
+let popupCurrentWaste = document.querySelector(".popup_type_waste");
+popupCurrentWaste.querySelector(".popup__dackground-transparent").addEventListener('click', closePopupCurrentWaste);
 
 let dropdownMenuButtonYear = document.querySelector("#dropdownMenuButtonYear");
 dropdownMenuButtonYear.addEventListener('click', setYearValues);
@@ -11,113 +16,7 @@ let dropdownMenuButtonMonth = document.querySelector("#dropdownMenuButtonMonth")
 dropdownMenuButtonMonth.addEventListener('click', setMonthValues);
 
 
-async function popupOpen() {
-    popup.classList.add("popup_opened");
-
-
-
-    let montDate = new Date(2023, 2);
-    const response = await fetch(`Waste/GetWastesOfOneDay?year=${montDate.getFullYear()}&month=${montDate.getMonth() + 1}&day=27`, {
-        method: "GET",
-        headers: { "Accept": "application/json" }
-    });
-    if (response.ok === true) {
-        // получаем данные
-        const wastes = await response.json();
-        let html = [];
-
-        for (let i = 0; i < wastes.length; i++) {
-            let goodDay = document.createElement("div");
-
-            goodDay.classList.add("calendar__day", "calendar__day-good_day");
-            goodDay.innerHTML =`
-                    <div class="calendar__day_text calendar__day_text-top">${wastes[i].value} ${wastes[i].category} ${wastes[i].comment}</div>`;
-            goodDay.onclick = popupOpen;
-
-            html.push(goodDay);
-        }
-       
-        let waistsHTML = document.querySelector(".waists_list");
-        waistsHTML.innerHTML = "";
-        html.forEach(day => {
-            waistsHTML.appendChild(day);
-        });
-    }
-}
-
-async function GetWastes() {
-    //Находим эелмент календарь
-    let calendar = document.querySelector(".calendar");
-    //добавляем эффект мерцания
-    calendar.classList.add("calendar_loader");
-
-    //находим месяц
-    let montDate = new Date(2023, 2);
-    let num = montDate.getDay();
-
-
-    let loader_html = '';
-
-    for (let i = 0; i < 35; i++) {
-        loader_html +=
-            `<div class="calendar__day calendar__day-bad_day">
-                <div class="calendar__day_text calendar__day_text-top"></div>
-                <div class="calendar__day_text calendar__day_text-bottom"></div>
-            </div>`;
-    }
-    calendar.innerHTML = loader_html;
-
-    // отправляет запрос и получаем ответ
-    const response = await fetch(`Waste/GetWastes?year=${montDate.getFullYear()}&month=${montDate.getMonth() + 1}`, {
-        method: "GET",
-        headers: { "Accept": "application/json" }
-    });
-
-    // если запрос прошел нормально
-    if (response.ok === true) {
-        // получаем данные
-        const wastes = await response.json();
-        let html = [];
-        for (let i = 1; i < num; i++) {
-            let badDay = document.createElement("div");
-            badDay.classList.add("calendar__day","calendar__day-bad_day");
-            badDay.innerHTML = `
-                <div class="calendar__day_text calendar__day_text-top"></div>
-                <div class="calendar__day_text calendar__day_text-bottom"></div>
-            `;
-            html.push(badDay);
-        }
-
-        for (let i = 0; i < wastes.length; i++) {
-            let goodDay = document.createElement("div");
-
-            goodDay.classList.add("calendar__day", "calendar__day-good_day");
-            goodDay.innerHTML =`
-                    <div class="calendar__day_text calendar__day_text-top">${i + 1}</div>
-                    <div class="calendar__day_text calendar__day_text-bottom">${wastes[i]}</div>`;
-            goodDay.onclick = popupOpen;
-
-            html.push(goodDay);
-        }
-        for (let i = 0; i < 35 - num + 1 - wastes.length; i++) {
-            let badDay = document.createElement("div");
-            badDay.classList.add("calendar__day","calendar__day-bad_day");
-            badDay.innerHTML = `<div class="calendar__day_text calendar__day_text-top"></div>
-                <div class="calendar__day_text calendar__day_text-bottom"></div>`;
-            html.push(badDay);
-        }
-
-        calendar.classList.remove("calendar_loader");
-
-        calendar.innerHTML = "";
-        html.forEach(day => {
-            calendar.appendChild(day);
-        });
-    }
-
-}
-
-
+let montDate = new Date(2023, 2);
 
 function setYearValues() {
     for (let year = new Date().getFullYear(); year >= 1920; year--) {
@@ -141,15 +40,139 @@ function setMonthValues() {
     new SimpleBar(document.querySelector("#dropdownMenuUlMonth"));
     this.removeEventListener('click', setMonthValues);
 }
+let wastesOfOneDay;
+async function PopupWasteListOpen() {
 
+    
+    let dayNum = this.children[0].firstChild.wholeText;
+    popupWasteList.querySelector(".popup__title").textContent=`${dayNum}.${montDate.getMonth() + 1}.${montDate.getFullYear()}`;
+    popupWasteList.classList.add("popup_opened");
+    
+     wastesOfOneDay = await GetWastesOfDay(dayNum);
+    let html = [];
+    
+    for (let i = 0; i < wastesOfOneDay.length; i++) {
+        let waste = document.createElement("div");
 
+        waste.classList.add("popup__watelist_item");
+        waste.setAttribute("id", i);
+        waste.innerHTML =`
+                <div class="align-self-center">${wastesOfOneDay[i].value}</div>
+                <div class="align-self-center">${wastesOfOneDay[i].category}</div>`;
+        waste.onclick=PopupCurrentWasteOpen;
+        html.push(waste);
+    }
+   
+    let waistsHTML = popupWasteList.querySelector(".waists_list");
+    waistsHTML.innerHTML = "";
+    html.forEach(waste => {
+        waistsHTML.appendChild(waste);
+    });
+    
+        new SimpleBar(waistsHTML);
+        
+    
+}
 
-function change(e) {
-    alert(this.value);
+function PopupCurrentWasteOpen(waste){
+    popupCurrentWaste.classList.add("popup_opened");
+ 
+
+}
+
+function closePopupWasteList(){
+
+    popupWasteList.classList.remove("popup_opened");
+}
+
+function closePopupCurrentWaste(){
+
+    popupCurrentWaste.classList.remove("popup_opened");
 }
 
 
-GetWastes();
+
+
+async function GetWastesByDays() {
+    //Находим эелмент календарь
+    
+    //добавляем эффект загрузки
+    SetCalendarLoader();
+    
+    let num = montDate.getDay();
+
+    // отправляет запрос и получаем ответ
+    const response = await fetch(`Waste/GetWastes?year=${montDate.getFullYear()}&month=${montDate.getMonth() + 1}`, {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+
+    // если запрос прошел нормально
+    if (response.ok === true) {
+        // получаем данные
+        const wastes = await response.json();
+        let html = [];
+        for (let i = 1; i < num; i++) {
+            let badDay = document.createElement("div");
+            badDay.classList.add("calendar__day","calendar__day-bad_day");
+            
+            html.push(badDay);
+        }
+
+        for (let i = 0; i < wastes.length; i++) {
+            let goodDay = document.createElement("div");
+
+            goodDay.classList.add("calendar__day", "calendar__day-good_day");
+            goodDay.innerHTML =`
+                    <div class="calendar__day_text calendar__day_text-top">${i + 1}</div>
+                    <div class="calendar__day_text calendar__day_text-bottom">${wastes[i]}</div>`;
+            goodDay.onclick = PopupWasteListOpen;
+
+            html.push(goodDay);
+        }
+        for (let i = 0; i < 35 - num + 1 - wastes.length; i++) {
+            let badDay = document.createElement("div");
+            badDay.classList.add("calendar__day","calendar__day-bad_day");
+            
+            html.push(badDay);
+        }
+
+        RemoveCalendarLoader();
+
+        calendar.innerHTML = "";
+        html.forEach(day => {
+            calendar.appendChild(day);
+        });
+    }
+
+}
+
+async function GetWastesOfDay(dayNum) {
+    const response = await fetch(`Waste/GetWastesOfOneDay?year=${montDate.getFullYear()}&month=${montDate.getMonth() + 1}&day=${dayNum}`, {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+    if (response.ok === true) {
+        return await response.json();
+    }
+
+}
 
 
 
+
+
+function SetCalendarLoader(){
+    for (let i = 0; i < 35; i++) {
+        calendar.appendChild(document.createElement("div"));
+        calendar.lastChild.classList.add("calendar__day","calendar__day-bad_day");
+    }
+    
+    calendar.classList.add("calendar_loader");
+}
+
+function RemoveCalendarLoader(){
+    calendar.classList.remove("calendar_loader");
+}
+
+GetWastesByDays();
